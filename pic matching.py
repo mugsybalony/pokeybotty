ebc19a25286c2b81093b4d1fa41679fb5dd42fd7
mzed86 <zafiropoulos.michael@gmail.com>
2016-09-29 16:20:37 +0100

    finds dealer

--- pic matching.py
+++ pic matching.py
@@ -2,20 +2,26 @@ import cv2
 import numpy as np
 from PIL import Image, ImageGrab, ImageDraw, ImageFilter
 import time
+import pyautogui
 
 
 def find_templates_on_screen(template,screenshot):
     res = cv2.matchTemplate(screenshot, template, cv2.TM_SQDIFF_NORMED)
     loc = np.where(res <= 0.005)
+    ##0.005 for card faces
     w, h = template.shape[1],template.shape[0]
-    #print(loc)
+    #Qprint(loc)
     #print(type(loc))
+    y = None
+    for x1 in range(len(loc[0])):
+        y = (loc[1][x1],loc[0][x1])
+        cv2.rectangle(screenshot, y, (y[0] + w, y[1] + h), (0, 0, 255), 2)
 
     for pt in zip(*loc[::-1]):
         #print(pt)
-     #   cv2.rectangle(screenshot, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        cv2.rectangle(screenshot, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-    #cv2.imwrite('res.png', screenshot)
+        cv2.imwrite('cards.png', screenshot)
 
         return pt
 
@@ -47,10 +53,64 @@ def cards_on_screen(screenshotpath):
     #print( len(cards))
     #print(cards)
 
-    return  [cards[a] for a in range(len(cards)) if cards[a][1] != None]
+    return [cards[a] for a in range(len(cards)) if cards[a][1] != None]
+
+def find_hidden_on_screen(template,screenshot):
+    res = cv2.matchTemplate(screenshot, template, cv2.TM_SQDIFF_NORMED)
+    loc = np.where(res <= 0.05)
+
+    w, h = template.shape[1],template.shape[0]
 
 
-screen = "ps/screenshot3.png"
+    y = None
+    numplayers = 0
+    for x1 in range(len(loc[0])):
+        y = (loc[1][x1],loc[0][x1])
+        numplayers +=1
+        cv2.rectangle(screenshot, y, (y[0] + w, y[1] + h), (0, 0, 255), 2)
+
+    cv2.imwrite('cards.png', screenshot)
+
+    return numplayers
+
+
+
+def find_dealer(screenpath):
+    dealerbutton = 'ps/dealerbutton.png'
+    screen = Image.open(screenpath)
+    screensh= cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB)
+    x,y, w ,h = pyautogui.locate(dealerbutton,screen)
+    #print box around dealer to show that it has been found
+    cv2.rectangle(screensh, ((x),(y)), (x+w,y+h), (0, 0, 255), 2)
+    cv2.imwrite('dealerfound.png', screensh)
+
+
+    return x,y #return x,y coordinates of dealer button
+
+
+
+
+
+
+screen = "ps/screenshot1.png"
 cs = cards_on_screen(screen)
 
+
 print(cs)
+
+
+cardpath = 'ps/backofcards2.png'
+
+screenshot1 = Image.open(screen)
+screenshot = cv2.cvtColor(np.array(screenshot1), cv2.COLOR_BGR2RGB)
+
+card = Image.open(cardpath)
+coveredcard = cv2.cvtColor(np.array(card), cv2.COLOR_BGR2RGB)
+
+x = find_hidden_on_screen(coveredcard,screenshot)
+
+print ("number of player is: %s" %x)
+
+dealerx, dealery = find_dealer(screen)
+
+print "dealer found at %s %s" %(dealerx,dealery)
